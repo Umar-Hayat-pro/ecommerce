035b486648f6d38609fc92971513882aafd1dc62
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2025-01-15 18:33:01 +0500

    Don't make a coategory for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Admin.create(email: "admin@example.com", password: "admin123456", password_confirmation: "admin123456")
